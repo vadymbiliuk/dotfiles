c38e38f08659400dfd7434f8989525fc5c48f2a7
Vadym Biliuk <vadym.biliuk@gmail.com>
2025-06-04 01:40:14 +0300

    feat: use snacks dashboard

--- nvim/lua/nvee/plugins/snacks.lua
+++ nvim/lua/nvee/plugins/snacks.lua
@@ -9,6 +9,33 @@ return {
   },
   opts = {
     animate = { enable = true },
+    dashboard = {
+      enable = true,
+      sections = {
+        { icon = " ", title = "Projects", section = "projects", padding = 2 },
+        { icon = " ", title = "Keymaps", section = "keys", padding = 2 },
+        { section = "startup" },
+      },
+      preset = {
+        pick = nil,
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session", action = "" },
+          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+        header = {},
+      },
+    },
     bigfile = { enable = true },
     git = { enable = true },
     gitbrowse = { enable = true },
