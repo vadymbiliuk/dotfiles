d0279c596448b1eef680146db289326e62e2e508
Vadym Biliuk <vadym.biliuk@gmail.com>
2025-11-01 17:54:55 +0200

    fix: list windows as boot option

--- nix/modules/nixos/lanzaboote.nix
+++ nix/modules/nixos/lanzaboote.nix
@@ -4,10 +4,19 @@
   environment.systemPackages = [ pkgs.sbctl ];
 
   boot.loader.systemd-boot.enable = lib.mkForce false;
+  boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.grub.enable = lib.mkForce false;
 
   boot.lanzaboote = {
     enable = true;
     pkiBundle = "/var/lib/sbctl";
+    configurationLimit = 10;
+  };
+
+  boot.loader.systemd-boot.extraEntries = {
+    "windows.conf" = ''
+      title Windows 11
+      efi /EFI/Microsoft/Boot/bootmgfw.efi
+    '';
   };
 }
