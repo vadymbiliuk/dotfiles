343ffd8e3e30b8a24f435014fd4101bfb13bc21b
Vadym Biliuk <vadym.biliuk@gmail.com>
2026-01-10 01:23:10 +0200

    fix(nixos): do not turn off screen, only lock

--- nix/modules/home/hypridle.nix
+++ nix/modules/home/hypridle.nix
@@ -20,15 +20,6 @@
           timeout = 600;
           on-timeout = "loginctl lock-session";
         }
-        {
-          timeout = 900;
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
-        }
-        {
-          timeout = 1800;
-          on-timeout = "systemctl suspend";
-        }
       ];
     };
   };
