4f2eabc427057984f8dc23ae8f2534691135b110
Vadym Biliuk <vadym.biliuk@gmail.com>
2026-01-21 20:43:41 +0200

    feat(nixos): fix sound issues

--- nix/modules/nixos/audio.nix
+++ nix/modules/nixos/audio.nix
@@ -10,7 +10,35 @@
     pulse.enable = true;
     jack.enable = true;
     wireplumber.enable = true;
-    
+    wireplumber.extraConfig."10-default-devices" = {
+      "wireplumber.settings" = {
+        "default.configured.audio.sink" = "easyeffects_sink";
+        "default.configured.audio.source" = "easyeffects_source";
+      };
+    };
+
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 1024;
+        "default.clock.min-quantum" = 1024;
+        "default.clock.max-quantum" = 2048;
+      };
+    };
+
+    extraConfig.pipewire-pulse."92-low-latency" = {
+      "pulse.properties" = {
+        "pulse.min.req" = "1024/48000";
+        "pulse.default.req" = "1024/48000";
+        "pulse.max.req" = "2048/48000";
+        "pulse.min.quantum" = "1024/48000";
+        "pulse.max.quantum" = "2048/48000";
+      };
+      "stream.properties" = {
+        "node.latency" = "1024/48000";
+      };
+    };
+
     extraConfig.pipewire.rtconfig = {
       "context.modules" = [
         {
@@ -31,8 +59,6 @@
       ];
     };
   };
-
-  programs.noisetorch.enable = true;
 
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
